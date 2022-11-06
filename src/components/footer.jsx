fd9d2b3047349958dc1282f6c0fb78bf04f54b41
McCoady <78250146+McCoady@users.noreply.github.com>
2022-11-06 22:42:20 +0000

    updated footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,16 @@
+import twitter from "../img/twitter.png";
+import discord from "../img/discord.png";
+import opensea from "../img/opensea.png";
+import etherscan from "../img/etherscan.png";
+import "./styles/footer.css";
+
 export default function Footer() {
     return (
-        <div>
-            Socials buttons here
+        <div className="footer">
+            <a href="https://www.twitter.com/CircolorsDAO"><img src={twitter} className="link_btn" alt="twitter link" /></a>
+            <a href="https://discord.gg/UJtkgAgxUV"><img src={discord} className="link_btn" alt="discord link" /></a>
+            <a href="https://www.opensea.io"><img src={opensea} className="link_btn" alt="opensea link" /></a>
+            <a href="https://www.etherscan.io"><img src={etherscan} className="link_btn" alt="etherscan link" /></a>
         </div>
     )
 }
--- src/routes/mint.jsx
+++ src/routes/mint.jsx
@@ -6,7 +6,11 @@ export default function Mint() {
         <div className="Mint">
             <Header />
             <Navbar />
-            <h2>Mint</h2>
+            <h2>Mint Date</h2>
+            <div>Mint Price: 0.0256eth</div>
+            <div># / 420 minted</div>
+            <div>Conditionally render site based on minting stage</div>
+            <div>Render latest mint here?</div>
             <Footer />
         </div>
     )
